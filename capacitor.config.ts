807e7b6f9f92379a072e6078902d14ee952149cf
LosaJR <280137368+LosaJR@users.noreply.github.com>
2026-09-14 18:55:51 +0200

    feat: añadir puente Android con Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.losajr.simplefinance',
+  appName: 'Simple Finance',
+  webDir: 'dist'
+};
+
+export default config;
